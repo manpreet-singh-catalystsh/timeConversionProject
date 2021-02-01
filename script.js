c9a027f05710da4defb0de6a73c1d2dde2458833
manpreet-singh-catalystsh <78337803+manpreet-singh-catalystsh@users.noreply.github.com>
2021-02-01 23:52:33 +0530

    v1.0
    fully functional and responsive
    
    seconds hand linear motion not fixed

--- script.js
+++ script.js
@@ -5,40 +5,99 @@ var clk2 = document.getElementById("clock2");
 var clk3 = document.getElementById("clock3");
 var clk4 = document.getElementById("clock4");
 
-//--------------clock face designing using canvas---------------------
+var dclk1 = document.getElementById("dclock1");
+var dclk2 = document.getElementById("dclock2");
+var dclk3 = document.getElementById("dclock3");
+var dclk4 = document.getElementById("dclock4");
 
+//--------------clock face designing using canvas---------------------
 var designc1 = clk1.childNodes[1].getContext("2d");
 var designc2 = clk2.childNodes[1].getContext("2d");
 var designc3 = clk3.childNodes[1].getContext("2d");
 var designc4 = clk4.childNodes[1].getContext("2d");
-designc1.fillStyle="#FFF";
-designc1.font = "20px Aria";
-designc1.fillText("India (GMT+5.30)",60,50);
 
-designc2.fillStyle="#FFF";
-designc2.font = "20px Arial";
-designc2.fillText("Sydney (GMT+11)",60,50);
+markers(designc1);
+markers(designc2);
+markers(designc3);
+markers(designc4);
 
-designc3.fillStyle="#FFF";
-designc3.font = "20px Arial";
-designc3.fillText("Siberia (GMT+7)",60,50);
 
-designc4.fillStyle="#FFF";
-designc4.font = "20px Arial";
-designc4.fillText("London (GMT)",60,50);
+
+function markers(design)
+{
+
+    design.lineWidth = 2;
+    design.strokeStyle="#FFF";
+    design.moveTo(150,0);
+    design.lineTo(150,8);
+    design.stroke();
+
+    design.moveTo(226,9);
+    design.lineTo(216,17);
+    design.stroke();
+
+    design.moveTo(285,35);
+    design.lineTo(267,42);
+    design.stroke();
+
+    
+    design.moveTo(76,9);
+    design.lineTo(86,17);
+    design.stroke();
+
+    design.moveTo(15,35);
+    design.lineTo(33,42);
+    design.stroke();
+
+    design.moveTo(300,75);
+    design.lineTo(280,75);
+    design.stroke();
+
+    design.moveTo(226,140);
+    design.lineTo(216,132);
+    design.stroke();
+//--------------------------------------------
+    design.moveTo(280,115);
+    design.lineTo(260,108);
+    design.stroke();
+
+    design.moveTo(150,150);
+    design.lineTo(150,140);
+    design.stroke();
+
+    design.moveTo(0,75);
+    design.lineTo(20,75);
+    design.stroke();
+//-----------------------------------------
+    
+
+
+    design.moveTo(75,140);
+    design.lineTo(85,130);
+    design.stroke();
+
+    design.moveTo(15,110);
+    design.lineTo(33,103);
+    design.stroke();
+}
+
+
+
+
+
+
 
 setInterval(()=>{
-                    //console.log(getTime(0));
-                    clockUpdate(clk1,getTime(0));
-                    clockUpdate(clk2,getTime(1));
-                    clockUpdate(clk3,getTime(2));
-                    clockUpdate(clk4,getTime(3));
+                    clockUpdate(clk1,dclk1,getTime(0));
+                    clockUpdate(clk2,dclk2,getTime(1));
+                    clockUpdate(clk3,dclk3,getTime(2));
+                    clockUpdate(clk4,dclk4,getTime(3));
                 },1000);
 
 
 
 ////function to update clock ui... arguments (clock id and time)
-function clockUpdate(clock,time)
+function clockUpdate(clock,dclock,time)
 {
     var sec = time.getSeconds();
     var min = time.getMinutes();
@@ -57,12 +116,8 @@ function clockUpdate(clock,time)
        hr=hr-12;
        notation="PM";
     }
-    var digitime=clock.childNodes[1].getContext("2d");
-    digitime.fillStyle="#66004b";
-    digitime.fillRect(20,80,150,30);
-    digitime.font = "20px Arial";
-    digitime.fillStyle="#FFF";
-    digitime.fillText(hr+":"+min+":"+sec+" "+notation,40,100);
+
+    dclock.innerHTML = (hr+" : "+min+" : "+sec+" "+notation);
 }
 
 
@@ -81,7 +136,6 @@ function getTime(clockno)
              return dt;
         case 1:
             dt.setMinutes(dt.getMinutes() + 11*60);         // for sydney
-            console.log(dt);
             return dt;
         case 2:
             dt.setMinutes(dt.getMinutes() + 7*60);          //for Syberia

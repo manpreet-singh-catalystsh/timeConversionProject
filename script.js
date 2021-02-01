67a1e2cec93166243390bf8e9c160775bc5115da
manpreet-singh-catalystsh <78337803+manpreet-singh-catalystsh@users.noreply.github.com>
2021-02-01 14:10:32 +0530

    basic clock layout
    basic clock layout created

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //will rotate clock hands at angles proportionately wrt time.
+
+//drawing clock
+document.getElementById("clock1").style.backgroundColor="#123";
+
+var c = document.getElementsByClassName("clockface")[0];
+var ctx = c.getContext("2d");
+ctx.fillStyle="#FFF";
+ctx.fillRect(c.width/2 -5,c.height/2 -5,10,10);
